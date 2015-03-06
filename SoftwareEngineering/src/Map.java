b2a026b28a1a3e46197d26293267723c3d117320
Jake <jshughes24@gmail.com>
2015-03-06 18:12:02 +0000

    Created project for initial interfaces
    Cell & Map interface started

---
+++ SoftwareEngineering/src/Map.java
@@ -0,0 +1,19 @@
+
+public interface Map {
+
+    public int[][] Cell;
+    public int sizeX = 0;
+    public int sizeY = 0;
+
+    public void generateMap();
+    
+    public Cell getCell(int x, int y);
+
+    public void setCell(int x, int y, Cell cell);
+
+
+
+
+}
+
+
