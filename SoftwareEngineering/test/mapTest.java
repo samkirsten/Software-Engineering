a54dbc80b59436b15a0091d814055b85df8fbea4
Jun <jrlee0701@gmail.com>
2015-03-18 18:30:54 +0000

    map test update

--- SoftwareEngineering/test/mapTest.java
+++ SoftwareEngineering/test/mapTest.java
@@ -4,49 +4,92 @@ import static org.junit.Assert.*;
 
 public class mapTest(){
 
-
 	@Test
 	public void testGenerateMap(){
-
+												// I think it doesn't need test.
 	}
 
 	@Test
-	public void testGetCellisRocky(){
+	public void testCellIsRocky(){
+		Map map = new Map();
+		map.generateMap();
+		Position p = new Position(1,1);
 
+		map.setCellIsRocky(p, true);
+		assertTrue(map.getCellIsRocky(p));
+
+		map.setCellIsRocky(p, false);
+		assertFalse(map.getCellIsRocky(p));
 	}
 
 	@Test
-	public void testSetCellIsRocky(){
+	public void testCellContents(){
+		Map map = new Map();
+		map.generateMap();
+		Position p = new Position(2,2);
 
+		assertEquals(map.getCellContents(p),".");	// defalut value;
+
+		map.setCellContents(p,"+");
+		assertEquals(map.getCellContents(p),"+");
+
+		map.setCellContents(p,"-");
+		assertEquals(map.getCellContents(p),"-");
+
+		map.setCellContents(p,"1");
+		assertEquals(map.getCellContents(p),"1");
+
+		map.setCellContents(p,"3");
+		assertEquals(map.getCellContents(p),"3");
+
+		map.setCellIsRocky(p, true);
+		assertTrue(map.getCellIsRocky(p));
+		assertEquals(map.getCellContents(p),"#");
 	}
 
-	@Test
-	public void testGetCellContents(){
+	@Test 									// need to discuss
+	public void testCellScentMarker(){
+		Map map = new Map();
+		map.generateMap();
+		Position p = new Position(2,2);
 
+		assertEquals(map.getCellScentMarker)
 	}
 
-	@Test
-	public void testSetCellContents(){
+	@Test 									// need to know default value of ant ID
+	public void testAntAtCell(){
+		Map map = new Map();
+		map.generateMap();
+		Position p = new Position(1,1);
 
-	}
+		map.setAntAtCell(p, 13);
+		assertTrue(map.getAntAtCell(p),13);
 
-	@Test
-	public void testGetCellScentMarker(){
-
-	}
-
-	@Test
-	public void testSetCellScentMarker(){
-
-	}
-
-	@Test
-	public void testGetAntAtCell(){
-		
-	}
-
-	@Test
-	public void testSetAntAtCell(){
-
+		map.setAntAtCell(p, 13);
+		assertFalse(map.getAntAtCell(p),12);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
