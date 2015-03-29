5175b62e4c8f0f3a0bcad5c8a32ad62c1d860b06
tina6397 <tina19951211@hotmail.com>
2015-03-29 14:58:28 +0100

    Update GUI

--- SoftwareEngineering/src/com/view/GuiTwoPlayerMenu.java
+++ SoftwareEngineering/src/com/view/GuiTwoPlayerMenu.java
@@ -1,8 +1,14 @@
 package com.view;
 
+import com.controller.ControllerImpl;
+import com.controller.GameController;
+import com.model.Game;
+import com.model.GameImpl;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.*;
 
 
@@ -10,8 +16,15 @@ public class GuiTwoPlayerMenu extends JFrame {
 
     JPanel panel;
     JLabel instruction;
-    JLabel p1Brain ;
-    JLabel p2Brain;
+    JLabel p1BrainStatus;
+    JLabel p2BrainStatus;
+
+    //files for loading in 2 brains
+    public File p1Brain ;
+    public File p2Brain ;
+
+    public Game singleGame;
+    public GameController singleController;
 
     Boolean p1Ready ;
     Boolean p2Ready ;
@@ -24,10 +37,13 @@ public class GuiTwoPlayerMenu extends JFrame {
     public final void initLayout() {
         //a label for instructions
         instruction = new JLabel("Please load your brains here, and press Start Game ", SwingConstants.CENTER);
+        //initialise game and controller
+        singleGame = new GameImpl();
+        singleController = new ControllerImpl();
 
         //initialise text lables
-        p1Brain = new JLabel("Player 1's Brain : Not loaded",SwingConstants.CENTER);
-        p2Brain = new JLabel("Player 2's Brain : Not loaded",SwingConstants.CENTER);
+        p1BrainStatus = new JLabel("Player 1's Brain : Not loaded",SwingConstants.CENTER);
+        p2BrainStatus = new JLabel("Player 2's Brain : Not loaded",SwingConstants.CENTER);
 
         //initialize booleans
         p1Ready = false ;
@@ -51,10 +67,11 @@ public class GuiTwoPlayerMenu extends JFrame {
                 //check for brain goes here. if OKAY, load, if not, tell user.
                 if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                     //System.out.println("Player 1 's brain : " + chooser.getSelectedFile());
-                    p1Brain.setText("Player1 's Brain : ["+chooser.getName(chooser.getSelectedFile())+" ]is loaded.");
+                    p1BrainStatus.setText("Player1 's Brain : [" + chooser.getName(chooser.getSelectedFile()) + " ]is loaded.");
+                    p1Brain = chooser.getSelectedFile() ;
                     p1Ready = true;
                 } else {
-                    p1Brain.setText("Your Brain is not working, retry! ");
+                    p1BrainStatus.setText("Your Brain is not working, retry! ");
                 }
 
             }
@@ -73,10 +90,12 @@ public class GuiTwoPlayerMenu extends JFrame {
                 //check for brain goes here. if OKAY, load, if not, tell user.
                 if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                     //System.out.println("Player 2 's brain : " + chooser.getSelectedFile());
-                    p2Brain.setText("Player2 's Brain : ["+chooser.getName(chooser.getSelectedFile())+" ]is loaded.");
+                    p2BrainStatus.setText("Player2 's Brain : [" + chooser.getName(chooser.getSelectedFile()) + " ]is loaded.");
+                    p2Brain = chooser.getSelectedFile() ;
+
                     p2Ready = true;
                 } else {
-                    p2Brain.setText("Your Brain is not working, retry! ");
+                    p2BrainStatus.setText("Your Brain is not working, retry! ");
                 }
 
                 //checking for the brain goes here
@@ -93,8 +112,11 @@ public class GuiTwoPlayerMenu extends JFrame {
                 // then runs game class
                 if (p1Ready && p2Ready) {
                     // GameGUI game = new GameGUI();
-                    GuiMainMenu m = new GuiMainMenu() ;
+                    //GuiMainMenu m = new GuiMainMenu() ;
+
                 }
+
+
 
             }
         });
@@ -102,8 +124,8 @@ public class GuiTwoPlayerMenu extends JFrame {
         panel.add(instruction);
         panel.add(player1);
         panel.add(player2);
-        panel.add(p1Brain);
-        panel.add(p2Brain);
+        panel.add(p1BrainStatus);
+        panel.add(p2BrainStatus);
         panel.add(startGame);
 
         setTitle("2-player Mode");
