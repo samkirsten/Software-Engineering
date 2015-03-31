c65bb419f62928c261a1f46ccdcdec79e64fb497
Jake <jshughes24@gmail.com>
2015-03-31 16:15:58 +0100

    GameController update

--- SoftwareEngineering/src/com/controller/ControllerImpl.java
+++ SoftwareEngineering/src/com/controller/ControllerImpl.java
@@ -1,45 +1,35 @@
-//package com.controller;
-//import com.model.*;
-//import com.view.GameGUI;
-//
-//import java.io.File;
-//import java.util.HashMap;
-//import java.util.List;
-//
-///**
-//* Created by Jacob on 28/03/2015.
-//*/
-//public class ControllerImpl implements GameController {
-//
-//    @Override
-//    public List<List<File>> createFixtures(List<File> brains, Tournament t) {
-//        return t.createFixtures(brains);
-//    }
-//
-//    @Override
-//    public Game createGame(File brain1, File brain2){
-//        Game game = new GameImpl(brain1,brain2);
-//        return game;
-//    }
-//
-//
-//    @Override
-//    public void nextRound(Game game) {
-//        game.nextRound();
-//    }
-//
-//    @Override
-//    public Game changeSides(Game game){
-//        File brain1 = game.getBrain(Colour.BLACK);
-//        File brain2 = game.getBrain(Colour.RED);
-//        Map map = game.getMap();
-//
-//        return new GameImpl(brain1,brain2,map);
-//    }
-//
-//    @Override
-//    public void updateScores(Game game, Tournament t) {
-//        t.updateScores(game);
-//    }
-//}
-//
+package com.controller;
+import com.model.*;
+import com.view.GameGUI;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+* Created by Jacob on 28/03/2015.
+*/
+public class ControllerImpl implements GameController {
+
+    @Override
+    public List<Game> createFixtures(HashMap<String,File> brains, Tournament t) {
+        return t.createFixtures(brains);
+    }
+
+    @Override
+    public Game createGame(File brain1,String name1, File brain2, String name2, GameGUI gui){
+        Game game = new GameImpl(brain1,name1,brain2,name2,gui);
+        return game;
+    }
+
+    @Override
+    public void startGame(Game game) {
+        game.start();
+    }
+
+    @Override
+    public void updateScores(Game game, Tournament t) {
+        t.updateScores(game);
+    }
+}
+
--- SoftwareEngineering/src/com/controller/GameController.java
+++ SoftwareEngineering/src/com/controller/GameController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import com.model.*;
+import com.view.GameGUI;
 
 import java.io.File;
 import java.util.HashMap;
@@ -8,19 +9,11 @@ import java.util.List;
 
 public interface GameController {
 
-    public List<List<File>> createFixtures(List<File> brains, Tournament t);
+    public List<Game> createFixtures(HashMap<String,File> brains, Tournament t);
 
-    public void nextRound(Game game);
+    public void startGame(Game game);
 
-    /**
-     * Returns a new game with the black and red anthills flipped on the same map
-     * @param game
-     * @return
-     */
-    public Game changeSides(Game game);
-
-    public Game createGame(File brain1, File brain2);
-
+    public Game createGame(File brain1, String name1, File brain2, String name2, GameGUI gui);
 
     /**
      * Updates the results of the game and appends it to the tournament objects
